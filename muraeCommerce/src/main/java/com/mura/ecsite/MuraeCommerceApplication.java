3b6fc0c052fb75dad927a793668763dbbed6ac03
hideo333666 <muramura031409@gmail.com>
2023-11-20 23:16:05 +0900

    初コミット

---
+++ muraeCommerce/src/main/java/com/mura/ecsite/MuraeCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.mura.ecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuraeCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuraeCommerceApplication.class, args);
+	}
+
+}
---
+++ muraeCommerce/src/test/java/com/mura/ecsite/MuraeCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mura.ecsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuraeCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
